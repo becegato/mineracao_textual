506f1b48bc5f08c1864150cdde7e5a9ec3711be6
Luísa Halley <88724262+becegato@users.noreply.github.com>
2023-02-26 19:12:41 -0300

    Update busca_padrãp_textual.py

--- busca_padrãp_textual.py
+++ busca_padrãp_textual.py
@@ -1,12 +1,12 @@
 import re
 
 def nomes():
-    #Exemplo de texto a ser minerado.
     simple_string = """Amy tem 5 anos, e sua irmã Mary tem 2 anos. 
+    Dr. Brown é o médico da família, e sua esposa Dra. Johnson é a dentista. 
     Ruth e Peter, seus pais, têm 3 filhos."""
 
     # Defina o padrão de expressão regular para encontrar os nomes
-    padrao = r'\b[A-Z][a-z]+\b'
+    padrao = r'(?<!\.)\b[A-Z][a-z]+\b'
 
     # Encontre todas as correspondências usando a função findall()
     lista_nomes = re.findall(padrao, simple_string)
